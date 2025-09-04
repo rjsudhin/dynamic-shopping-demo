726598645f4ffeb14ee14a53ec2770e3de00478e
SudhinRj <rjsudhin@users.noreply.github.com>
2025-09-04 13:27:09 +0530

    can't fixed the but of dynamically updated count of wishes when it created

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ function addingItemsInList() {
 
 function creatingWishList(item) {
   let li = document.createElement('li')
+  let listCountElement = document.createElement('span')
+  listCountElement.classList.add('count')
   let span = document.createElement('span')
   span.textContent = item
   span.classList.add('wish-item')
@@ -34,9 +36,10 @@ function creatingWishList(item) {
   deleteButton.textContent = 'delete'
   deleteButton.classList.add('delete-btn')
   deleteButton.addEventListener('click', removeCurrentItem)
-  li.append(span, deleteButton)
+  li.append(listCountElement, span, deleteButton)
   menu.appendChild(li)
   checkingWishListCount()
+  addingCountNumbers()
 }
 
 
@@ -49,6 +52,8 @@ function removeCurrentItem(e) {
   // remove the current wish list element
   menu.removeChild(parentElement)
 
+  addingCountNumbers()
+
   // when any item removed in less the the limit count it will be adding for next inventory
   if (menu.childElementCount < 5) {
     addItemBtn.disabled = false
@@ -59,9 +64,28 @@ function removeCurrentItem(e) {
 
 // checking the wish list's count and added limit to add new inventory
 function checkingWishListCount() {
-  console.log(menu.childElementCount)
   if (menu.childElementCount >= 5) {
     addItemBtn.disabled = true
     userInput.disabled = true
   }
+}
+
+
+function addingCountNumbers() {
+  let currentLength = menu.childElementCount
+  let currentCountElements = Array.from(document.querySelectorAll('.count'))
+  // for (i = 0; i < currentCountElements.length; i++) {
+  //   let currentNumber = currentLength
+  //   console.log('curent Length is now ' + currentNumber)
+  //   console.log(currentCountElements[i])
+  //   currentCountElements[i].textContent = currentNumber
+  // }
+
+  for (let i = 0; i < currentLength; i++) {
+    for (let j = 0; j < currentCountElements.length; j++) {
+      currentCountElements[j].textContent = i + 1
+    }
+  }
+
+  
 }
